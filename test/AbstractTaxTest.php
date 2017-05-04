765fe9a586724db94fc7fc484359b8c1a8f827e1
Alberto Clemente <albertoclemente1983@gmail.com>
2017-05-04 17:57:25 +0000

    Refactored tests

--- test/AbstractTaxTest.php
+++ test/AbstractTaxTest.php
@@ -2,53 +2,9 @@
 namespace VoidInsight\Taxtotum\Test;
 
 use PHPUnit\Framework\TestCase;
-use VoidInsight\Taxtotum\Libs\AbstractTax;
-use VoidInsight\Taxtotum\Libs\TaxInterface;
-use VoidInsight\Taxtotum\Libs\TaxConfInterface;
+
 
 class AbstractTaxTest extends TestCase {
     
-    protected $obj;
-    
-    protected function setUp() {
-        $this->obj = $this->getMockForAbstractClass(AbstractTax::class);
-    }
-    
-    /**
-     * @dataProvider paramsDataProvider
-     */
-    public function testParamsAreSettable($param, $value) {
-        $this->assertInstanceOf(TaxConfInterface::class, $this->obj);
-        
-        $this->assertSame($this->obj, $this->obj->setParamValue($param, $value));
-        $this->assertSame($value, $this->obj->getParamValue($param));
-    }
-    
-    public function paramsDataProvider() {
-        return [
-            ['test_param', 'test_value']
-        ];
-    }
-    
-    /**
-     * @dataProvider taxableDataProvider
-     * @depends testParamsAreSettable
-     */
-    public function testTaxableAttributeIsSettable($taxable) {
-        
-        $this->assertInstanceOf(AbstractTax::class, $this->obj);
-        
-        $this->assertSame($this->obj, $this->obj->setTaxable($taxable));
-        
-        $this->assertSame($taxable,
-                $this->obj->getTaxable());
-    }
-    
-    public function taxableDataProvider() {
-        return [
-            [0.0],
-            [10.0],
-            [100000.99]
-        ];
-    }
+    use AbstractTaxTestTrait;
 }
---
+++ test/AbstractTaxTestTrait.php
@@ -0,0 +1,48 @@
+<?php
+namespace VoidInsight\Taxtotum\Test;
+
+use VoidInsight\Taxtotum\Libs\AbstractTax;
+use VoidInsight\Taxtotum\Libs\TaxInterface;
+use VoidInsight\Taxtotum\Libs\TaxConfInterface;
+
+trait AbstractTaxTestTrait {
+    
+    /**
+     * @dataProvider paramsDataProvider
+     */
+    public function testParamsAreSettable($param, $value) {
+        $sut = $this->getMockForAbstractClass(AbstractTax::class);
+        
+        $this->assertInstanceOf(TaxConfInterface::class, $sut);
+        
+        $this->assertSame($sut, $sut->setParamValue($param, $value));
+        $this->assertSame($value, $sut->getParamValue($param));
+    }
+    
+    public function paramsDataProvider() {
+        return [
+            ['test_param', 'test_value']
+        ];
+    }
+    
+    /**
+     * @depends testParamsAreSettable
+     * 
+     * @dataProvider taxableDataProvider
+     */
+    public function testTaxableAttributeIsSettable($taxable) {
+        $sut = $this->getMockForAbstractClass(AbstractTax::class);
+        
+        
+        $this->assertSame($sut, $sut->setTaxable($taxable));
+        $this->assertSame($taxable, $sut->getTaxable());
+    }
+    
+    public function taxableDataProvider() {
+        return [
+            [0.0],
+            [10.0],
+            [100000.99]
+        ];
+    }
+}
