943c66762c27e8efcb025c9ca1a4d4cea1c42145
Alberto Clemente <albertoclemente1983@gmail.com>
2017-05-05 12:02:07 +0000

    Removed Dummy Strategy, use Ratio with 1.0 as ratio

--- test/TaxStrategy/DummyTaxStrategyTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace VoidInsight\Taxtotum\Test\TaxStrategy;
-
-use PHPUnit\Framework\TestCase;
-
-class DummyTaxStrategyTest extends TestCase {
-    
-    use DummyTaxStrategyTestTrait;
-}
